296c7b22b4a33645a5c0c16d5365d9c5cb10cf59
Clayton Ponte <ccponte@gmail.com>
2023-02-05 20:21:10 +0000

    Darts exercise

---
+++ src/main/java/com/ccponte/Darts.java
@@ -0,0 +1,13 @@
+package com.ccponte;
+
+class Darts {
+    int score(double xOfDart, double yOfDart) {
+
+        double distance = Math.sqrt(Math.pow(xOfDart, 2) + Math.pow(yOfDart, 2));
+
+        return distance <= 1 ? 10 :
+               distance <= 5 ? 5:
+               distance <= 10 ? 1 : 0;
+
+    }
+}
