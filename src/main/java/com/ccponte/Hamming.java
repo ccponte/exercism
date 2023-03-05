f64bd9cad056650c05f085116713ab6248ac829f
Clayton Ponte <ccponte@gmail.com>
2023-03-05 21:41:41 +0000

    Hamming exercise

---
+++ src/main/java/com/ccponte/Hamming.java
@@ -0,0 +1,32 @@
+package com.ccponte;
+
+import java.util.stream.IntStream;
+
+public class Hamming {
+
+    private final String leftStrand;
+    private final String rightStrand;
+
+    public Hamming(String leftStrand, String rightStrand) {
+        if (!leftStrand.isEmpty() && rightStrand.isEmpty()) {
+            throw new IllegalArgumentException("right strand must not be empty.");
+        }
+
+        if (leftStrand.isEmpty() && !rightStrand.isEmpty()) {
+            throw new IllegalArgumentException("left strand must not be empty.");
+        }
+
+        if (leftStrand.length() != rightStrand.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+
+        this.leftStrand = leftStrand;
+        this.rightStrand = rightStrand;
+    }
+
+    public int getHammingDistance() {
+        return (int) IntStream.range(0, leftStrand.length())
+                              .filter(index -> leftStrand.toCharArray()[index] != rightStrand.toCharArray()[index])
+                              .count();
+    }
+}
